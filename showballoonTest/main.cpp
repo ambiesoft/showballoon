4d91d47d56371f0ed52709a9a029dcf311664e81
gradiator <gradiator@pina>
2017-10-16 02:55:00 +0900

    shell_notifyicon is bad

--- showballoonTest/main.cpp
+++ showballoonTest/main.cpp
@@ -41,10 +41,11 @@ int main()
 	testit(command.c_str());
 	
 
-	command=string_format(_T("/title:%s /icon:%s /defaulticon /balloonicon:%d %s"),
+	command=string_format(_T("/title:%s /icon:%s /defaulticon /balloonicon:%d /duration:%d %s"),
 		_T("%E3%82%82%E3%82%89%E3%81%84%E6%B3%A3%E3%81%8D"), //title
 		(wstring(_T("\"") + wstring(szSB) + _T("\""))).c_str(), // icon
 		1, // balloonicon
+		60*1000,
 		_T("%E3%81%88%E3%81%88%E3%81%84%E3%81%82%E3%81%82+%E5%90%9B%E3%81%8B%E3%82%89%E3%80%8C%E3%82%82%E3%82%89%E3%81%84%E6%B3%A3%E3%81%8D%E3%80%8D+%E3%81%BB%E3%82%8D%E3%82%8A%E3%83%BB%E3%81%BB%E3%82%8D%E3%82%8A+%E3%81%B5%E3%81%9F%E3%82%8A%E3%81%BC%E3%81%A3%E3%81%A1")
 		);
 	testit(command.c_str());
@@ -54,7 +55,7 @@ int main()
 		_T("%EC%98%81%EC%96%B4"), //title
 		(wstring(_T("\"") + wstring(szSB) + _T("\""))).c_str(), // icon
 		1, // balloonicon
-		20*1000,
+		60*1000,
 		_T("%E8%8B%B1%E8%AF%AD%EF%BC%88English%EF%BC%8C%E8%81%86%E8%81%BDi%2F%CB%88%C9%AA%C5%8B%C9%A1l%C9%AA%CA%83%2F%EF%BC%89%E6%98%AF%E4%B8%80%E7%A7%8D%E8%A5%BF%E6%97%A5%E8%80%B3%E6%9B%BC%E8%AF%AD%E8%A8%80")
 		);
 	testit(command.c_str());
